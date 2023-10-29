35ecff5c1556f0c8d1f7c3d5300affc882161603
jiyuujin <jiyuujinunite@gmail.com>
2023-10-29 20:48:46 +0900

    mobile optimization

--- _includes/BlogPosts.tsx
+++ _includes/BlogPosts.tsx
@@ -2,7 +2,7 @@ import type { Page } from "lume/core.ts";
 import { BlogPost } from "./BlogPost.tsx";
 
 export const BlogPosts = ({ posts }: { posts: Page[] }) => (
-  <div className="max-w-screen-sm px-6 mx-auto">
+  <div className="max-w-screen px-6 mx-auto">
     <ul className="pt-16 lt-sm:pt-12 border-t-1 border-gray-300/80">
       {posts.map((post, key) => (
         <li key={key} className="my-3">
