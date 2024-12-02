38b47abe3edea04f66c7bf072ac3973e5ec2bb51
jiyuujin <jiyuujinunite@gmail.com>
2024-12-02 22:13:28 +0900

    feat: icons

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 import type { Data } from "lume/core.ts";
 import { title } from "./_config.ts";
 import { BlogPosts } from "./_includes/BlogPosts.tsx";
-import { getPosts } from "./_includes/utils.tsx";
+import { getIcons, getPosts } from "./_includes/utils.tsx";
+import { Icon } from "./_includes/Icon.tsx";
 
 export const layout = "base.tsx";
 
@@ -15,6 +16,17 @@ export default (_: Data) => {
           <h1 className="mt-3 text-4xl text-gray-900 font-bold">{title}</h1>
         </div>
       </header>
+      <div className="max-w-screen px-6">
+        <ul className="flex justify-center pt-4 lt-sm:pt-2">
+          {getIcons().map((icon, key) => (
+            <li key={key} className="mx-3 my-3">
+              <a href={icon.url} target="_blank">
+                <Icon name={icon.name} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <BlogPosts posts={posts} />
     </>
   );
