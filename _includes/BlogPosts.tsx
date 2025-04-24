19c436f75aceee160df99d4d1122e108fb14ddac
jiyuujin <y-kitamura@wevnal.co.jp>
2025-04-25 07:52:29 +0900

    deno fmt --check

--- _includes/BlogPosts.tsx
+++ _includes/BlogPosts.tsx
@@ -3,7 +3,10 @@ import { BlogPost } from "./BlogPost.tsx";
 
 export const BlogPosts = ({ posts }: { posts: Page[] }) => (
   <div className="max-w-screen px-6">
-    <ul id="blogMain" className="pt-16 lt-sm:pt-12 border-t-1 border-gray-300/80">
+    <ul
+      id="blogMain"
+      className="pt-16 lt-sm:pt-12 border-t-1 border-gray-300/80"
+    >
       {posts.map((post, key) => (
         <li key={key} className="my-3">
           <BlogPost post={post} />
